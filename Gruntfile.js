f4b7432036ae3d4f9f4f7a8413b8edcddffcce44
Thanh <thanh@dongbat.com>
2013-12-28 14:39:06 +0700

    added git ignore, package.json, grunt

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';',
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      dist: {
+        src: ['lib/utils/*.js', 'lib/*.js', 'lib/systems/*.js', 'lib/manager/*.js'],
+        dest: '<%= pkg.name %>.js'
+      }
+    },
+    jshint: {
+      beforeconcat: ['lib/utils/*.js', 'lib/*.js', 'lib/systems/*.js', 'lib/manager/*.js'],
+      afterconcat: ['<%= pkg.name %>.js']
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'jshint']);
+};
